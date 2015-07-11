dce0a3e88c96f08ce9b11c6edae59fae18792455
Kenny Pflug <kenny.pflug@live.de>
2015-07-11 11:35:12 +0200

    added functionality to copy to a file

--- DependencyInversionPrinciple.ConsoleApp/Program.cs
+++ DependencyInversionPrinciple.ConsoleApp/Program.cs
@@ -1,23 +1,60 @@
 ﻿using System;
+using System.IO;
 
 namespace DependencyInversionPrinciple.ConsoleApp
 {
+    public class File
+    {
+        private static StreamWriter _streamWriter;
+
+        public static void InitializeStreamWriter(string filePath)
+        {
+            _streamWriter = new StreamWriter(filePath);
+        }
+
+        public static void Write(char character)
+        {
+            _streamWriter.Write(character);
+        }
+
+        public static void Dispose()
+        {
+            _streamWriter.Close();
+            _streamWriter = null;
+        }
+    }
+
+    public enum Target
+    {
+        Console,
+        File
+    }
+
     class Program
     {
         static void Main()
         {
-            Copy();
+            Copy(Target.File);
         }
 
-        private static void Copy()
+        private static void Copy(Target target)
         {
+            if (target == Target.File)
+                File.InitializeStreamWriter("Text.txt");
+
             while(true)
             {
                 var consoleKeyInfo = Console.ReadKey(true);
                 if (consoleKeyInfo.Key == ConsoleKey.Escape)
-                    return;
-                Console.Write(consoleKeyInfo.KeyChar);
+                    break;
+                if (target == Target.Console)
+                    Console.Write(consoleKeyInfo.KeyChar);
+                else if (target == Target.File)
+                    File.Write(consoleKeyInfo.KeyChar);
             }
+
+            if (target == Target.File)
+                File.Dispose();
         }
     }
 }
