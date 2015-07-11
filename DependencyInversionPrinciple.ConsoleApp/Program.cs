18d00257645c4c6a7d3a401faf65acf0271441c5
Kenny Pflug <kenny.pflug@live.de>
2015-07-11 12:29:52 +0200

    added filewriter as IWriter dependency to composition root, the corresponding object is disposed automatically by the DI container

--- DependencyInversionPrinciple.ConsoleApp/Program.cs
+++ DependencyInversionPrinciple.ConsoleApp/Program.cs
@@ -4,22 +4,24 @@ namespace DependencyInversionPrinciple.ConsoleApp
 {
     class Program
     {
+        private const string FilePath = "Text.txt";
+
         static void Main()
         {
             // Composition Root
             // We're using a DI container now
             var serviceContainer = new ServiceContainer();
             serviceContainer.Register<IReader, ConsoleReader>();
-            serviceContainer.Register<IWriter, ConsoleWriter>();
+            serviceContainer.Register<IWriter>(f => new FileWriter(FilePath), new PerRequestLifeTime());
             serviceContainer.Register<CopyProcess>();
 
             // Run the actual program
             // Obtain the root object of the object graph that is necessary to run this program
-            var copyProcess = serviceContainer.GetInstance<CopyProcess>();
-            copyProcess.Execute();
-
-            // Teardown
-            //writer.Dispose(); Not necessary for ConsoleWriter
+            using (serviceContainer.BeginScope())
+            {
+                var copyProcess = serviceContainer.GetInstance<CopyProcess>();
+                copyProcess.Execute();
+            } // Teardown happens here
         }
     }
 }
