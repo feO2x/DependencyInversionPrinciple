2748d152ab151ebbd1fd9d944c95fd17fe3d24b0
Kenny Pflug <kenny.pflug@live.de>
2015-07-11 11:25:38 +0200

    Added initial implementation of copy

--- DependencyInversionPrinciple.ConsoleApp/Program.cs
+++ DependencyInversionPrinciple.ConsoleApp/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace DependencyInversionPrinciple.ConsoleApp
+﻿using System;
+
+namespace DependencyInversionPrinciple.ConsoleApp
 {
     class Program
     {
         static void Main()
         {
+            Copy();
+        }
+
+        private static void Copy()
+        {
+            while(true)
+            {
+                var consoleKeyInfo = Console.ReadKey(true);
+                if (consoleKeyInfo.Key == ConsoleKey.Escape)
+                    return;
+                Console.Write(consoleKeyInfo.KeyChar);
+            }
         }
     }
 }
